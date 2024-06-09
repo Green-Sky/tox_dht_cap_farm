903a9794bccf9260ac9e9dce03789bc3e5f1d886
Green Sky <green@g-s.xyz>
2024-06-09 11:41:57 +0200

    start project setup

---
+++ tool_src/main.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <chrono>
+
+// needs a custom toxcore build, since the api is private
+
+// starts and bootstraps toxcore (new)
+// idles for 5min
+// grabs the dht numbers and appends them to csv
+
+int main(int argc, char** argv) {
+
+
+
+
+	return 0;
+}
+
